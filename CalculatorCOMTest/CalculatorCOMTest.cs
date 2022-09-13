278601c17195111fa8e1edbffaf481216c51a1ad
aabir <shblnmn@gmail.com>
2022-09-13 18:21:47 +0800

    New unit test added.

--- CalculatorCOMTest/CalculatorCOMTest.cs
+++ CalculatorCOMTest/CalculatorCOMTest.cs
@@ -5,42 +5,39 @@ namespace CalculatorCOMTest
 {
     public class CalculatorCOMTest
     {
+        private readonly CalculatorCOM.CalculatorCOM _calculator;
         private readonly Mock<ICalculatorCOM> _mock;
+
         public CalculatorCOMTest()
         {
+            _calculator = new CalculatorCOM.CalculatorCOM();
             _mock = new Mock<ICalculatorCOM>();
         }
         [Fact]
         public void Sum_With_Expected_Result()
         {
-            int val1 = 5;
-            int val2 = 2;
-            int expected = 7;
-
-            //arrange
-            _mock.Setup(x => x.Sum(val1, val2)).Returns(7);
-
-            //act 
-            int result = _mock.Object.Sum(val1, val2);
-
-            //assert
+            var val1 = 5;
+            var val2 = 2;
+            var expected = 7;
+            var result = _calculator.Sum(val1, val2);
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10, -5)]
+        [InlineData(-5, 5)]
+        public void Sum_Throws_Exception(int val1, int val2)
+        {
+            Assert.Throws<ArgumentException>(() => _calculator.Sum(val1, val2));
         }
 
         [Fact]
         public void Sum_With_Incorrect_Result()
         {
-            int val1 = 8;
-            int val2 = 5;
-            int expected = 11;
-
-            //arrange
-            _mock.Setup(x => x.Sum(val1, val2)).Returns(13);
-
-            //act 
-            int result = _mock.Object.Sum(val1, val2);
-
-            //assert
+            var val1 = 8;
+            var val2 = 5;
+            var expected = 11;
+            var result = _calculator.Sum(val1, val2);
             Assert.NotEqual(expected, result);
         }
 
@@ -48,6 +45,15 @@ namespace CalculatorCOMTest
         public void ToJSON_With_Correct_Object()
         {
             var businessObj = SampleBusinessObj();
+            var path = @"C:\\01WORKSPACE\\04IRASSG\\BusinessObj.json";
+
+            //_mock.Setup(x => x.ToJSON(businessObj, path)).Verifiable();
+        }
+
+        [Fact]
+        public void ToJSON_With_Invalid_Object()
+        {
+            var businessObj = InvalidBusinessObj();
             var path = "C:\\01WORKSPACE\\04IRASSG\\BusinessObj.json";
             _mock.Setup(x => x.ToJSON(businessObj, path)).Verifiable();
         }
@@ -57,6 +63,21 @@ namespace CalculatorCOMTest
             var businessObj = new BusinessObj()
             {
                 BusinessName = "November Rain Corp.",
+                RegistrationNumber = "XYZ987",
+                SubmissionDate = new SubmissionDate()
+                {
+                    Day = 09,
+                    Month = 09,
+                    Year = 2024
+                }
+            };
+            return businessObj;
+        }
+
+        private BusinessObj InvalidBusinessObj()
+        {
+            var businessObj = new BusinessObj()
+            {
                 RegistrationNumber = "XYZ987",
                 SubmissionDate = new SubmissionDate()
                 {
